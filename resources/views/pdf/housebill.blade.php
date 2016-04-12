fbd8dd1105f2b6404d37b6578c79b6868751b18e
Nhu Dương Tấn <duongtannhu@gmail.com>
2016-04-12 07:42:55 +0700

    Update house bill.

--- resources/views/pdf/housebill.blade.php
+++ resources/views/pdf/housebill.blade.php
@@ -2,70 +2,133 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <style type="text/css">
+        .header_title {
+            width: 100%;
+            text-align: center;
+            height: 16px;
+            line-height: 16px;
+            font-size: 16px;
+        }
+        .header_subtitle {
+            font-size: 6px;
+            height: 8px;
+            line-height: 6px;
+        }
+        .ptn {
+            padding-top: 0 !important;
+        }
+        .pbn {
+            padding-bottom: 0 !important;
+        }
+        .mtn {
+            margin-top: 0 !important;
+        }
+        .mbn {
+            margin-bottom: 0 !important;
+        }
+        .bdt {
+            border-top: solid 1px;
+        }
+        .bdr {
+            border-right: solid 1px;
+        }
+        .bdl {
+            border-left: solid 1px;
+        }
+        .bdb {
+            border-bottom: solid 1px;
+        }
+        td {
+            text-align: left;
+            vertical-align: top;
+            margin: 0;
+            padding: 0;
+        }
+        p {
+            margin: 0;
+            padding: 0;
+        }
+        .caption_1 {
+            font-size: 10px;
+        }
+        .content_1 {
+            
+        }
+        .content_2 {
+            font-size: 9px;
+        }
+        .height-200 {
+            height: 80px;
+        }
+        .pl2 {
+            padding-left: 2px;
+        }
+    </style>
 </head>
 <body>
-    <h4>Ocean Bill of Lading</h4>
-    <p>(not negotiable unless consigned to order)</p>
-    <table>
+    <h4 class="header_title pbn mtn mbn">Ocean Bill of Lading</h4>
+    <p class="header_title header_subtitle ptn">(not negotiable unless consigned to order)</p>
+    <table class="bdt">
         <tr>
-            <td colspan="3">
-                <p>Shipper</p>
-                <p>LIEN A CO., LTD.<br/>55/1A KHUONG VIET STREET, PHU TRUNG WARD,<br/>TAN PHU DISTRICT, HO CHI MINH CITY, VIETNAM</p>
+            <td class="bdb" colspan="3">
+                <p class="caption_1">Shipper</p>
+                <p class="content_1">LIEN A CO., LTD.<br/>55/1A KHUONG VIET STREET, PHU TRUNG WARD,<br/>TAN PHU DISTRICT, HO CHI MINH CITY, VIETNAM</p>
             </td>
-            <td rowspan="6" width="44%">
+            <td class="bdl bdb pl2" rowspan="6" width="44%">
                 <h3>PHUC LOC FORWARDING CO.</h3>
-                <p>RECEIVED by the carrier the goods as specified above in apparent good order and condition unless otherwise stated, to be transported to such place as agreed, authorised or permitted herein and subject to all the terms and conditions appearing on the front and reverse of this Bill of Lading to which the Merchant agrees by accepting this Bill of Lading any local privileges and customs notwithstanding. The particular given above as stated by the shipper and the weight, measure, quantity, condition, contents and value of the goods are unknown to the Carrier.</p>
-                <p>Note:</p>
-                <p>The Merchant's attention is called to the fact that according to clause 10,11 and 12 of this Bill of Lading, the liability of the Carrier is, in most cases, limited in respect of the loss of or damage to the goods and delay.</p>
-                <p>LAW AND JURISDICTION CLAUSE</p>
-                The contract evidenced by or contained in this Bill of Lading shall be governed by the law of the Singapore and any claim or dispute arising hereunder or in connection herewith shall (without prejusdice to the Carrier's right to commence proceeding in any other jurisdiction) be subject to the jurisdiction of the Courts of Singapore.
+                <p class="content_2">RECEIVED by the carrier the goods as specified above in apparent good order and condition unless otherwise stated, to be transported to such place as agreed, authorised or permitted herein and subject to all the terms and conditions appearing on the front and reverse of this Bill of Lading to which the Merchant agrees by accepting this Bill of Lading any local privileges and customs notwithstanding. The particular given above as stated by the shipper and the weight, measure, quantity, condition, contents and value of the goods are unknown to the Carrier.</p>
+                <p class="content_2">Note:</p>
+                <p class="content_2">The Merchant's attention is called to the fact that according to clause 10,11 and 12 of this Bill of Lading, the liability of the Carrier is, in most cases, limited in respect of the loss of or damage to the goods and delay.</p>
+                <p class="content_2">LAW AND JURISDICTION CLAUSE</p>
+                <p class="content_2">The contract evidenced by or contained in this Bill of Lading shall be governed by the law of the Singapore and any claim or dispute arising hereunder or in connection herewith shall (without prejusdice to the Carrier's right to commence proceeding in any other jurisdiction) be subject to the jurisdiction of the Courts of Singapore.</p>
             </td>
         </tr>
         <tr>
-            <td colspan="3">
-                <p>Consignee</p>
-                <p>PHIL. AUSTRALIAN BRASS BED<br/>11 2ND AVE., STA MARIA IND.ESTATE, BAGUMBAYAN TAGUIG CITY, PHILIPPINES 1631</p>
+            <td class="bdb" colspan="3">
+                <p class="caption_1">Consignee</p>
+                <p class="content_1">PHIL. AUSTRALIAN BRASS BED<br/>11 2ND AVE., STA MARIA IND.ESTATE, BAGUMBAYAN TAGUIG CITY, PHILIPPINES 1631</p>
             </td>
         </tr>
         <tr>
-            <td colspan="3">
-                <p>Notify Party</p>
-                <p>SAME AS CNEE</p>
+            <td class="bdb height-200" colspan="3">
+                <p class="caption_1">Notify Party</p>
+                <p class="content_1">SAME AS CNEE</p>
             </td>
         </tr>
         <tr>
-            <td colspan="2">
-                <p>Pre-carriage by</p>
-                <p>&nbsp;</p>
+            <td class="bdb bdr" colspan="2">
+                <p class="caption_1">Pre-carriage by</p>
+                <p class="content_1">&nbsp;</p>
             </td>
-            <td>
-                <p>Place of receipt</p>
-                <p>HO CHI MINH, VIETNAM</p>
-            </td>
-        </tr>
-        <tr>
-            <td width="24%">
-                <p>Ocean Vessel</p>
-                <p>BALTIC STRAIT</p>
-            </td>
-            <td width="8%">
-                <p>Voy. No</p>
-                <p>044B</p>
-            </td>
-            <td width="24%">
-                <p>Port of loading</p>
-                <p>HO CHI MINH (CAT LAI)</p>
+            <td class="bdb">
+                <p class="caption_1">Place of receipt</p>
+                <p class="content_1">HO CHI MINH, VIETNAM</p>
             </td>
         </tr>
         <tr>
-            <td>
-                <p>Port of discharge</p>
-                <p>MANILA NORTH HARBOUR</p>
+            <td class="bdb bdr" width="24%">
+                <p class="caption_1">Ocean Vessel</p>
+                <p class="content_1">BALTIC STRAIT</p>
             </td>
-            <td>&nbsp;</td>
-            <td>
-                <p>Place of Delivery</p>
-                <p>MANILA, METRO MANILA, PHILIPPINES</p>
+            <td class="bdb bdr" width="8%">
+                <p class="caption_1">Voy. No</p>
+                <p class="content_1">044B</p>
+            </td>
+            <td class="bdb" width="24%">
+                <p class="caption_1">Port of loading</p>
+                <p class="content_1">HO CHI MINH (CAT LAI)</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="bdb bdr">
+                <p class="caption_1">Port of discharge</p>
+                <p class="content_1">MANILA NORTH HARBOUR</p>
+            </td>
+            <td class="bdb bdr">&nbsp;</td>
+            <td class="bdb">
+                <p class="caption_1">Place of Delivery</p>
+                <p class="content_1">MANILA, METRO MANILA, PHILIPPINES</p>
             </td>
         </tr>
     </table>
