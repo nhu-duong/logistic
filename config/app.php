9138ff8013eb8ce6a99c6eda20b9ebc911af0c44
Nhu Duong <nhu.duong@swissitbridge.com>
2016-04-27 17:24:24 +0700

    Use new plugin for html.

--- config/app.php
+++ config/app.php
@@ -147,8 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
-        'Illuminate\Html\HtmlServiceProvider',
         'Barryvdh\DomPDF\ServiceProvider',
         'Barryvdh\Snappy\ServiceProvider',
         /*
